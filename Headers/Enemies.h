bd939c3feaa74d8aec230fcafbe4c7542fba2f49
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2021-10-22 19:31:05 -0300

    update map ajust

---
+++ Headers/Enemies.h
@@ -0,0 +1,16 @@
+//
+// Created by Usuario on 22/10/2021.
+//
+
+#ifndef MAIN_CPP_ENEMIES_H
+#define MAIN_CPP_ENEMIES_H
+
+
+
+class Enemies {
+
+};
+
+
+
+#endif //MAIN_CPP_ENEMIES_H
---
+++ SourceFiles/Enemies.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Usuario on 22/10/2021.
+//
+
+#include "Enemies.h"
